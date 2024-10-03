4a5a71fa13e25ed620b0b15610642a7baf766309
Nicolas Brichet <nicolas.brichet@quantstack.net>
2024-10-03 12:11:22 +0200

    Use the YRoom task group for asyncrhonous call

--- pycrdt_websocket/yroom.py
+++ pycrdt_websocket/yroom.py
@@ -305,7 +305,11 @@ class YRoom:
         except Exception as exception:
             self._handle_exception(exception)
 
-    async def local_update_awareness(self, state):
+    def local_update_awareness(self, state: bytes):
+        if self._task_group is not None:
+            self._task_group.start_soon(self._local_update_awareness, state)
+
+    async def _local_update_awareness(self, state: bytes):
         try:
             async with create_task_group() as tg:
                 for client in self.clients:
